b191cd539ee29cfb418e0929900bbf26d339b102
emreicoz07 <ramazanemreicoz@gmail.com>
2024-10-11 20:19:20 +0200

    backend update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,8 +39,9 @@ app.get('/', (req, res) => {
 // Sunucuyu başlatıyoruz
 const port = process.env.PORT || 10000;
 app.listen(port, async () => {
+  await pool.query(`DROP TABLE users`);
   await pool.query(`CREATE TABLE IF NOT EXISTS users(
-    id   integer NOT NULL PRIMARY KEY,  
+    id SERIAL PRIMARY KEY,  
     email   varchar(255) NOT NULL,
     password varchar(255),
     balance int NOT NULL DEFAULT 20
